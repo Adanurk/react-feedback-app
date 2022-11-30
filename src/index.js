ab4710c7b8f5743a5c5bcc7c734578d78c2e7098
Adanurk <gumussoykbr@gmail.com>
2022-11-30 23:16:06 +0100

    btnDisabled validation added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React, { StrictMode } from "react";
+// with strict mode react offers additional warnings and checks.
 import ReactDOM from "react-dom/client";
+// react dom library allows us to basically interact with document object model in the browser
 import App from "./App";
 import "./index.css";
+// our global css file
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <StrictMode>
     <App />
